f5e9a291e9b75a79c39345a0a4226a7555f5e537
Roman Burdun <rburdun@gmail.com>
2020-06-04 08:43:28 +0100

    Unit tests for postsService added.

---
+++ src/test/java/com/bloggie/server/fixtures/TestFixtures.java
@@ -0,0 +1,78 @@
+package com.bloggie.server.fixtures;
+
+import com.bloggie.server.domain.Post;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class TestFixtures {
+
+    public static List<Post> getPosts() {
+
+        Post postOne = new Post();
+        postOne.setId(1L);
+        postOne.setDateCreated( LocalDateTime.now());
+        postOne.setDateUpdated( LocalDateTime.now());
+        postOne.setPostTitle("Test post one title");
+        postOne.setContent("Test post one content");
+        postOne.setCover("testPostOne.webp");
+        postOne.setReadTime(10);
+        postOne.setSlug("test-post-one");
+        postOne.setPublicationDate(LocalDateTime.now());
+
+
+        Post postTwo = new Post();
+        postTwo.setId(2L);
+        postTwo.setDateCreated( LocalDateTime.now());
+        postTwo.setDateUpdated( LocalDateTime.now());
+        postTwo.setPostTitle("Test post two title");
+        postTwo.setContent("Test post two content");
+        postTwo.setCover("testPostTwo.webp");
+        postTwo.setReadTime(25);
+        postTwo.setSlug("test-post-two");
+        postTwo.setPublicationDate(LocalDateTime.now());
+
+        Post postThree = new Post();
+        postThree.setId(3L);
+        postThree.setDateCreated( LocalDateTime.now());
+        postThree.setDateUpdated( LocalDateTime.now());
+        postThree.setPostTitle("Test post three title");
+        postThree.setContent("Test post three content");
+        postThree.setCover("testPostThree.webp");
+        postThree.setReadTime(5);
+        postThree.setSlug("post-three");
+
+        return Arrays.asList(postOne, postTwo, postThree);
+    }
+
+    public static Post getSinglePost() {
+        Post post = new Post();
+        post.setId(1L);
+        post.setDateCreated( LocalDateTime.now());
+        post.setDateUpdated( LocalDateTime.now());
+        post.setPostTitle("Test post title");
+        post.setContent("Test post content");
+        post.setCover("testPost.webp");
+        post.setReadTime(10);
+        post.setSlug("test-post");
+        post.setPublicationDate(LocalDateTime.now());
+
+        return post;
+    }
+
+    public static Post getUpdatedPost() {
+        Post post = new Post();
+        post.setId(1L);
+        post.setDateCreated( LocalDateTime.now());
+        post.setDateUpdated( LocalDateTime.now());
+        post.setPostTitle("Test post title updated");
+        post.setContent("Test post content updated");
+        post.setCover("testPost.webp");
+        post.setReadTime(10);
+        post.setSlug("test-post-updated");
+        post.setPublicationDate(LocalDateTime.now());
+
+        return post;
+    }
+}
---
+++ src/test/java/com/bloggie/server/services/PostsServiceTest.java
@@ -0,0 +1,95 @@
+package com.bloggie.server.services;
+
+import com.bloggie.server.api.v1.mappers.PostMapper;
+import com.bloggie.server.api.v1.models.PostDTO;
+import com.bloggie.server.api.v1.models.PostUpdateDTO;
+import com.bloggie.server.domain.Post;
+import com.bloggie.server.fixtures.TestFixtures;
+import com.bloggie.server.repositories.PostsRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoSettings;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+class PostsServiceTest {
+
+    @Mock
+    private PostsService postsService;
+    @Mock
+    private PostsRepository postsRepository;
+    private PostMapper postMapper = PostMapper.INSTANCE;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        postsService = new PostsServiceImpl(postsRepository, postMapper);
+    }
+
+
+    @Test
+    void createPost() {
+
+        Post post = new Post();
+        post.setPostTitle("Test post title");
+        post.setContent("Test post content");
+        post.setCover("testPost.webp");
+        post.setReadTime(10);
+        post.setSlug("test-post");
+        post.setPublicationDate(LocalDateTime.now());
+
+        Mockito.when(postsRepository.save(any(Post.class))).thenReturn(TestFixtures.getSinglePost());
+
+        PostDTO createdPost = postsService.createPost(postMapper.postToPostDto(post));
+
+        assertNotNull(createdPost);
+        assertNotNull("test-post", createdPost.getSlug());
+
+    }
+
+    @Test
+    void getPosts() {
+
+        Mockito.when(postsRepository.findAll()).thenReturn(TestFixtures.getPosts());
+        List<PostDTO> posts = postsService.getPosts();
+        assertNotNull(posts);
+        assertEquals(3, posts.size());
+
+    }
+
+    @Test
+    void deletePostBySlug() {
+
+        Mockito.when(postsRepository.findBySlug(any(String.class))).thenReturn(Optional.of(TestFixtures.getSinglePost()));
+        PostDTO deletedPost = postsService.deletePostBySlug("test-post");
+        assertNotNull(deletedPost);
+        assertEquals("test-post", deletedPost.getSlug());
+    }
+
+    @Test
+    void updatePostBySlug() {
+
+        PostUpdateDTO update = new PostUpdateDTO();
+        update.setPostTitle("Test post title updated");
+        update.setContent("Test post content updated");
+        update.setSlug("test-post-updated");
+
+        Mockito.when(postsRepository.findBySlug(any(String.class))).thenReturn(Optional.of(TestFixtures.getSinglePost()));
+        Mockito.when(postsRepository.save(any(Post.class))).thenReturn(TestFixtures.getUpdatedPost());
+
+        PostDTO updatedPost = postsService.updatePostBySlug("test-post", update);
+
+        assertNotNull(updatedPost);
+        assertEquals("test-post-updated", updatedPost.getSlug());
+
+
+
+    }
+}
