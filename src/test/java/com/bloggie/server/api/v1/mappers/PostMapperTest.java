bbb224c9d70eb8dfd9d288d0316fa96fab8e0b89
Roman Burdun <rburdun@gmail.com>
2020-06-14 22:12:39 +0100

    PostMapper tests updated.

--- src/test/java/com/bloggie/server/api/v1/mappers/PostMapperTest.java
+++ src/test/java/com/bloggie/server/api/v1/mappers/PostMapperTest.java
@@ -2,6 +2,7 @@ package com.bloggie.server.api.v1.mappers;
 
 import com.bloggie.server.api.v1.models.PostDTO;
 import com.bloggie.server.api.v1.models.PostExcerptDTO;
+import com.bloggie.server.api.v1.models.PostUpdateDTO;
 import com.bloggie.server.domain.Post;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -72,6 +73,25 @@ class PostMapperTest {
         assertEquals("Test title", postExcerptDTO.getTitle());
         assertEquals("test.webp", postExcerptDTO.getCover());
         assertEquals(10, postExcerptDTO.getReadTime());
+
+    }
+
+    @Test
+    void postUpdateDtoToPost() {
+
+        PostUpdateDTO postUpdateDTO = new PostUpdateDTO();
+        postUpdateDTO.setTitle("DTO test title");
+        postUpdateDTO.setContent("DTO test content");
+        postUpdateDTO.setCover("testDto.webp");
+        postUpdateDTO.setReadTime(5);
+
+        Post post = postMapper.postUpdateDtoToPost(postUpdateDTO);
+
+        assertNotNull(post);
+        assertEquals("DTO test title", post.getTitle());
+        assertEquals("DTO test content", post.getContent());
+        assertEquals("testDto.webp", post.getCover());
+        assertEquals(5, post.getReadTime());
 
     }
 }
