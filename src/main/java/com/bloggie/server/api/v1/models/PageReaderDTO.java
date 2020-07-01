5f2d3ff769afc1b3a4e86faee0357472d341bc25
Roman Burdun <rburdun@gmail.com>
2020-07-01 20:34:31 +0100

    Removed dateUpdated and dateCreated attributes from PageReaderDTO.

--- src/main/java/com/bloggie/server/api/v1/models/PageReaderDTO.java
+++ src/main/java/com/bloggie/server/api/v1/models/PageReaderDTO.java
@@ -3,14 +3,11 @@ package com.bloggie.server.api.v1.models;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Setter
 @Getter
 public class PageReaderDTO {
-    private LocalDateTime dateCreated;
-    private LocalDateTime dateUpdated;
     private String title;
     private String content;
     private String slug;
